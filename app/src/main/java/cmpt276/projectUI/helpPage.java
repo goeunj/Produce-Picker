fd24997f45cd9561e36c1c74129f015ab031fb37
goeunj <goeunj@sfu.ca>
2020-06-25 10:35:31 -0700

    Initial UI page.
    All initial pages/screens needed for the app is created.

---
+++ app/src/main/java/cmpt276/projectUI/helpPage.java
@@ -0,0 +1,16 @@
+package cmpt276.projectUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cmpt276.project.R;
+
+public class helpPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_page);
+    }
+}
---
+++ app/src/main/java/cmpt276/projectUI/scorePage.java
@@ -0,0 +1,16 @@
+package cmpt276.projectUI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import cmpt276.project.R;
+
+public class scorePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_page);
+    }
+}
