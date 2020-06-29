aa331c8cfbaa3dd91a1a058df141f683a52479be
Jacob <jacobathanna@gmail.com>
2020-06-29 12:10:46 -0700

    Splash screen done

--- app/src/main/java/cmpt276/projectUI/splashScreen.java
+++ app/src/main/java/cmpt276/projectUI/splashScreen.java
@@ -2,7 +2,17 @@ package cmpt276.projectUI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import cmpt276.project.R;
 
@@ -12,5 +22,49 @@ public class splashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Button skip = findViewById(R.id.btnSkip);
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), menuPage.class));
+            }
+        });
+
+
+        TextView authorView = findViewById(R.id.txtAuthors);
+        Animation authorAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.author_anim);
+        TextView titleView = findViewById(R.id.txtTitle);
+        Animation titleAnim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.title_anim);
+
+
+        titleView.setAnimation(titleAnim);
+        authorView.setAnimation(authorAnim);
+
+
+        authorAnim.setAnimationListener(new Animation.AnimationListener() {
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        finish();
+                        startActivity(new Intent(getApplicationContext(), menuPage.class));
+                    }
+                }, 2000);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 }
